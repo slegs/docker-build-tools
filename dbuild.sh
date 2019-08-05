defad53e51da244a9f67d7e72a2addc77a30affd
Paul Walsh <paulgwalsh@gmail.com>
2019-08-05 12:21:45 +0100

    Fixed dbuild

--- dbuild.sh
+++ dbuild.sh
@@ -3,11 +3,11 @@
 function usage {
         cat <<EOM
 
-Docker and Git Build tool for Docker Images by Paul Walsh
+Docker and Git Build tool for Docker Images
 
-Create symbolic links for 
+Create symbolic links for
 - drelease.sh to /usr/local/bin/drelease
-- dbuild.sh to /usr/local/bin/dbuild   
+- dbuild.sh to /usr/local/bin/dbuild
 
 Call from build directory where your Dockerfile is located
 
@@ -20,7 +20,7 @@ Usage: $(basename "$0") [OPTION]...
   -u VALUE    Docker username (required)
   -i VALUE    Docker image name (required)
   -v VALUE    version number (required)
-  -t VALUE    build type. 
+  -t VALUE    build type.
               if set to PROD will create build with tag stable-x.x.x and latest
               else will assume DEV and create build with tag dev-x.x.x and test
   -r          runs the build in Docker
@@ -94,14 +94,15 @@ if [ "$TYPE" == "PROD" ] ; then
 	docker build -t $USERNAME/$IMAGE:stable-$VERSION -t $USERNAME/$IMAGE:latest .
 
 	if [ "$RUN_BUILD" == "YES" ] ; then
-        	docker run $USERNAME/$IMAGE:stable-$VERSION 
+    docker run $USERNAME/$IMAGE:stable-$VERSION
 	fi
 
 else
 
-        docker build -t $USERNAME/$IMAGE:dev-$VERSION  -t $USERNAME/$IMAGE:test .
-        if [ "$RUN_BUILD" == "YES" ] ; then
-	        docker run $USERNAME/$IMAGE:dev-$VERSION
+  docker build -t $USERNAME/$IMAGE:dev-$VERSION -t $USERNAME/$IMAGE:test .
+
+  if [ "$RUN_BUILD" == "YES" ] ; then
+    docker run $USERNAME/$IMAGE:dev-$VERSION
 	fi
 
 fi
